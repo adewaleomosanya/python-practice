2152aa2f060f22c31ea016e4e53b0de86cb905e2
Omosanya Akeem <akeemomosanya98@gmail.com>
2023-09-27 18:06:27 +0100

    corrections

--- Mad-Libs/main.py
+++ Mad-Libs/main.py
@@ -1,54 +1,54 @@
 Title = input("Title of your story: ")
 
 temp = """
-Once upon a time in a {Adjective} {Noun}, there lived a {Adjectivess} {Noun} named {Name}. 
-{Name} was known for being incredibly {Adverb} when it came to {Verb}. One {adjective} day, 
-{Pronoun} met a {Adjectives} {Animal} who said, "{Exclamation}! Let's play {Noun}!"
-{Name} agreed, and together, they had {adjectives} adventures. 
-They even enjoyed a meal of {Food} while watching the {Adjectiv} {Noun} at {Place}.
-{Name} learned that sometimes, the best adventures happen when you {Verb} and enjoy the world around you.
+Once upon a time in a {Adjective} {Noun}, there lived a {adjective2} {Noun2} named {Name}. 
+{Name} was known for being incredibly {Adverb} when it came to {Verb}. One {adjective3} day, 
+{Pronoun} met a {Adjective4} {Animal} who said, "{Exclamation}! Let's play {Noun3}!"
+{Name} agreed, and together, they had {adjective5} adventures. 
+They even enjoyed a meal of {Food} while watching the {Adjective6} {Noun4} at {Place}.
+{Name} learned that sometimes, the best adventures happen when you {Verb2} and enjoy the world around you.
 """
 
 Adjective = input("Enter an Adjective: ")
 Noun = input("Enter a Noun: ")
-Adjectivess = input("Enter an Ajectivess: ")
-Non = input("Enter a Noun: ")
+adjective2 = input("Enter an Ajectivess: ")
+Noun2 = input("Enter a Noun: ")
 Name = input("Enter a Name: ")
 Adverb = input("Enter an Adverb: ")
 Verb = input("Enter a Verb: ")
-adjective = input("Enter an Adjective: ")
+adjective3 = input("Enter an Adjective: ")
 Pronoun = input("Enter a Pronoun: ")
-Adjectives = input("Enter an Adjective: ")
+Adjective4 = input("Enter an Adjective: ")
 Animal = input("Enter an Animal: ")
 Exclamation = input("Enter an Exclamation: ")
-Nun = input("Enter a Noun: ")
-adjectives = input("Enter an Adjective: ")
+Noun3 = input("Enter a Noun: ")
+adjective5 = input("Enter an Adjective: ")
 Food = input("Enter a Food: ")
-Adjectiv = input("Enter an Adjective: ")
-Nouns = input("Enter a Noun: ")
+Adjective6 = input("Enter an Adjective: ")
+Noun4 = input("Enter a Noun: ")
 Place = input("Enter a Place: ")
-Verbs = input("Enter a Verb: ")
+Verb2 = input("Enter a Verb: ")
 
 story = temp.format(
     Adjective=Adjective,
     Noun=Noun,
-    Adjectivess=Adjectivess,
-    Non=Non, 
+   adjective2=adjective2, 
+    Noun2=Noun2, 
     Name=Name,
     Adverb=Adverb,
     Verb=Verb,
-    adjective=adjective, 
+    adjective3=adjective3, 
     Pronoun=Pronoun,
-    Adjectives=Adjectives,
+    Adjective4=Adjective4,
     Animal=Animal,
     Exclamation=Exclamation,
-    Nun=Nun,
-    adjectives=adjectives,
+    Noun3=Noun3,
+    adjective5=adjective5,
     Food=Food,
-    Adjectiv=Adjectiv,
-    Nouns=Nouns,
+    Adjective6=Adjective6,
+    Noun4=Noun4,
     Place=Place,
-    Verbs=Verbs,
+    Verb2=Verb2,
 )
 
 print(Title)
