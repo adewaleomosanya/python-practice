6c6ce063826c4b7303ab1d2a091b0a3b567f70b7
Omosanya Akeem <akeemomosanya98@gmail.com>
2023-10-01 16:21:01 +0100

    NGgame.py

--- Number-Guessing/NGgame.py
+++ Number-Guessing/NGgame.py
@@ -1,22 +1,33 @@
 import random
 
-secret_number = random.randint(1, 10)
+number = random.randint(1, 10)
 
 print("Welcome to the NGgame!")
 attempts = 0
-
 while True:
-    try:
-        user_guess = int(input("Guess the number between 1 and 10: "))
-        attempts += 1
+        try:
+            usernum = int(input("Guess a number between 1 and 10: "))
+            attempts += 1
 
-        if user_guess < secret_number:
-            print("It's lower, try again.")
-        elif user_guess > secret_number:
-            print("It's higher, try again.")
-        else:
-            print(f"Nice! You got it!")
-            print(f"It took you {attempts} attempts.")
-            break
-    except ValueError:
-        print("Invalid input. Please enter a valid number between 1 and 10.")
+            if usernum < number:
+               print("It's lower, try again.")
+            elif usernum > number:
+                    
+                    print("It's higher, try again.")
+            else:
+                print(f"Nice! You got it!")
+                print(f"It took you {attempts} attempts.") 
+            if usernum == number:
+              play_again =input ("Would you like to play again Yes/No: ")
+            if play_again != "Yes":
+                 print("It was nice playing with you")
+                 break
+            else:
+                usernum
+        except :
+            if usernum == ValueError:
+               print("Invalid input. Please enter a valid number between 1 and 10.")
+        
+          
+
+
